8e9a108929cd390010598568d5c5ac1d5f67cd26
Mohamad-bigdeli <mohamadbigdeli24@gmail.com>
2025-02-27 01:22:44 +0330

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -81,23 +81,24 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME", default="db"),
-        "USER": config("DB_USER", default="admin"),
-        "PASSWORD": config("DB_PASSWORD", default="admin"),
-        "HOST": config("DB_HOST", default="db"),
-        "PORT": config("DB_PORT", default="5432"),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": config("DB_NAME", default="db"),
+            "USER": config("DB_USER", default="admin"),
+            "PASSWORD": config("DB_PASSWORD", default="admin"),
+            "HOST": config("DB_HOST", default="db"),
+            "PORT": config("DB_PORT", default="5432"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
