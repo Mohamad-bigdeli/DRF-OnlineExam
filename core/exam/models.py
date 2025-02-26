3323ba0b00d18a1560951418d19120423a1e51a4
Mohamad-bigdeli <mohamadbigdeli24@gmail.com>
2025-02-27 01:04:30 +0330

    reformating

--- core/exam/models.py
+++ core/exam/models.py
@@ -6,7 +6,6 @@ User = get_user_model()
 
 # Create your models here.
 
-
 class Category(models.Model):
     title = models.CharField(max_length=225, unique=True)
     created = models.DateTimeField(auto_now_add=True)
